31d718b7b41df6afc744e74661ccc6675fa24035
Danyil Marchuk <zemero58@gmail.com>
2023-02-17 15:55:03 +0100

    add the bool that stores the information if the user finished typing numbers

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -7,18 +7,13 @@
 
 import UIKit
 import Neumorphic
+import NeumorphismKit
 
 class ViewController: UIViewController {
 
+    private var isFinishedTypingNumber: Bool = true
     @IBOutlet weak var displayLabel: UILabel!
     
-
-    @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-    }
-    
-    @IBAction func numButtonPressed(_ sender: UIButton) {
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
